24576264eec9f4dc1ac12b1f2d7205f61fe330e4
Martin Klasson <martin@hejamartin.se>
2023-11-23 11:37:02 +0100

    Moves out constants, needed in "wp-config"-level.

--- wordpress-plugin-stena-websolutions.php
+++ wordpress-plugin-stena-websolutions.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Wordpress Utilities | Web Solutions
 Description: Generic helper functions to adhere to our Wordpress sites.
-Version: 1.1.1
+Version: 1.1.2 [untagged/unreleased]
 Author: Web Solutions
 */
 
@@ -19,25 +19,6 @@ class Main
         AntiSpamBot::init();
 
         /*
-            Does: removes the concat of scripts
-            Why: vulnerable for DOS-attacks. This also requires denying with htaccess
-            Read more: https://nvd.nist.gov/vuln/detail/CVE-2018-6389
-        */
-        define('CONCATENATE_SCRIPTS', false);
-
-        /*
-            Does: ManageWP needs this for php8> to be able to take backups etc
-            Why: Backup did not work, and ticket make with ManageWP gave us this solution
-        */
-        define('FS_METHOD', 'direct');
-
-        /*
-            Does: ManageWP needs this for php8> to be able to take backups etc
-            Why: Backup did not work, and ticket make with ManageWP gave us this solution
-        */
-        define('WP_MEMORY_LIMIT', getenv('PHP_MEMORY_LIMIT') ?: '512M');
-
-        /*
             Does: disable xmlrpc
             Why: vulnerable to hack attacks, like DDoS and brute force attacks. The PHP file also tends to use up a lot of your server resources, making your website super slow.
             Read more: https://www.scottbrownconsulting.com/2020/03/two-ways-to-fully-disable-wordpress-xml-rpc/
@@ -49,5 +30,6 @@ class Main
         add_filter('xmlrpc_methods', 'remove_xmlrpc_methods');
     }
 }
+
 
 Main::init();
