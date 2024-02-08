6df37cbda175a8c068bd8d73fb356a36ea1ad8ac
Martin Klasson <martin@hejamartin.se>
2024-02-08 10:10:08 +0100

    Adds extra comment, as well as version bump

--- wordpress-plugin-stena-websolutions.php
+++ wordpress-plugin-stena-websolutions.php
@@ -2,11 +2,13 @@
 /*
 Plugin Name: Wordpress Utilities | Web Solutions
 Description: Generic helper functions to adhere to our Wordpress sites.
-Version: 1.1.2
+Version: 1.1.4
 Author: Web Solutions
 */
 
 namespace SwsHelperPlugin;
+
+if (!defined('ABSPATH')) exit;
 
 require_once __DIR__ . '/classes/antispambot.php';
 
@@ -22,12 +24,12 @@ class Main
             Does: disable xmlrpc
             Why: vulnerable to hack attacks, like DDoS and brute force attacks. The PHP file also tends to use up a lot of your server resources, making your website super slow.
             Read more: https://www.scottbrownconsulting.com/2020/03/two-ways-to-fully-disable-wordpress-xml-rpc/
+            
+            Every site should already use the method of disabling xmlrpc in the .htaccess, but this is another fallback for the same thing.
         */
-        function remove_xmlrpc_methods($methods)
-        {
+        add_filter('xmlrpc_methods', function ($methods) {
             return [];
-        }
-        add_filter('xmlrpc_methods', 'remove_xmlrpc_methods');
+        });
     }
 }
 
